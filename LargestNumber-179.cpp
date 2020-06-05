5451d1c8ed3ee56709feb92333ceb09532b783bf
Virinchi-12 <57909196+Virinchi-12@users.noreply.github.com>
2020-06-05 16:36:28 +0530

    Update LargestNumber-179.cpp

--- LargestNumber-179.cpp
+++ LargestNumber-179.cpp
@@ -3,27 +3,11 @@ public:
     string largestNumber(vector<int>& nums) {
         
         int n=nums.size(),i;
-        
-        /*sort(nums.begin(),nums.end(),[](int &a, int &b){
-            
-            float x=a,y=b;
-            while(x>=1){
-                x=x/10;
-            }
-            while(y>=1)
-            {
-                y=y/10;
-            }
-            cout<<x<<" "<<y<<" ";*/
-        
+             
         sort(nums.begin(),nums.end(),[](int &a, int &b){
             string s1=to_string(a),s2=to_string(b);
             int q=s1.size(),w=s2.size();
-            //int i=0;
-            /*if(s1[0]==s2[0] && s1[i++]>s2[i++])
-                return q>w;
-            else*/
-            
+         
             if(s1+s2>s2+s1)
                 return true;
             else return false;
