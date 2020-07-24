51067af4c44294d7bc8fe619f534f54a3c6a84d0
Virinchi-12 <57909196+Virinchi-12@users.noreply.github.com>
2020-07-24 15:38:52 +0530

    Create House_Robber-198.cpp

---
+++ House_Robber-198.cpp
@@ -0,0 +1,67 @@
+/*
+198. House Robber
+Easy
+
+4870
+
+149
+
+Add to List
+
+Share
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+ 
+
+Constraints:
+
+0 <= nums.length <= 100
+0 <= nums[i] <= 400
+*/
+
+//Commented out part is easier(bottom-up)
+
+class Solution {
+public:
+    int memoize(vector<int> &dp,int i,vector<int>nums){
+        if(i<0) return 0;
+    
+        if(dp[i]>=0) return dp[i];
+        
+        int r=max(memoize(dp,i-2,nums)+nums[i],memoize(dp,i-1,nums));
+        dp[i]=r;
+        return r;
+    }
+    int rob(vector<int>& nums) {
+        // int n=nums.size(),i;
+        // if(n==0) return 0;
+        // if(n==1) return nums[0];
+        //  vector<int>dp(n,-1);
+        // dp[0]=nums[0];
+        // dp[1]=max(nums[0],nums[1]);
+        // for(i=2;i<n;i++){
+        //     dp[i]=max(dp[i-2]+nums[i],dp[i-1]);
+        // }
+        // return dp[n-1];
+        
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return memoize(dp,n-1,nums);
+    }
+};
